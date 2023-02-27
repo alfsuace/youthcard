4b2aa248c2e0c3cb551e56c9bd40b9cb2a0b33a3
alfsuace <alfonso.suaace@educa.jcyl.es>
2023-02-27 19:39:37 +0100

    Creacion de Bussines

---
+++ src/com/alfsuace/youthCard/domain/models/Bussines.java
@@ -0,0 +1,31 @@
+package com.alfsuace.youthCard.domain.models;
+
+public class Bussines {
+    private String id;
+    private String name;
+    private String cif;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCif() {
+        return cif;
+    }
+
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+}
