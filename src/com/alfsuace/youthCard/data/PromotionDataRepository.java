fd476f9f3c51de3f72d825307c3c26e6d5c6246f
alfsuace <alfonso.suaace@educa.jcyl.es>
2023-02-27 18:48:35 +0100

    Implementacion PromotionDataRepository

--- src/com/alfsuace/youthCard/data/PromotionDataRepository.java
+++ src/com/alfsuace/youthCard/data/PromotionDataRepository.java
@@ -1,5 +1,27 @@
 package com.alfsuace.youthCard.data;
 
-public class PromotionDataRepository {
+import com.alfsuace.youthCard.data.local.PromotionFileLocalDataSource;
+import com.alfsuace.youthCard.domain.models.Promotion;
+import com.alfsuace.youthCard.domain.useCase.PromotionRepository;
 
+import java.util.List;
+
+public class PromotionDataRepository implements PromotionRepository {
+
+    private PromotionFileLocalDataSource promotionFileLocalDataSource;
+
+    public PromotionDataRepository(PromotionFileLocalDataSource promotionFileLocalDataSource){
+        this.promotionFileLocalDataSource=promotionFileLocalDataSource;
+    }
+
+
+    @Override
+    public List<Promotion> getAll() {
+        return null;
+    }
+
+    @Override
+    public void save(Promotion promotion) {
+
+    }
 }
